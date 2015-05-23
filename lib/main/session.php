0cebb6b6cd58ad7e45103adb237122cad9c190db
talent4636@gmail.com <talent4636@gmail.com>
2015-05-24 01:04:16 +0800

    add admin router

---
+++ lib/main/session.php
@@ -0,0 +1,17 @@
+<?php
+
+class lib_main_session{
+    
+    public function set(){
+        
+    }
+    
+    public function get(){
+        
+    }
+    
+    public function check(){
+        
+    }
+    
+}
---
+++ view/public/js/main_admin.js
@@ -0,0 +1,3 @@
+/**
+ * Created by xiejun on 2015/5/14.
+ */
